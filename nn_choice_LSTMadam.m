54f1cd53c414c9e15da56b693a76ca908a253d7c
UmikazeC <128567813+UmikazeC@users.noreply.github.com>
2024-07-31 15:32:44 +0800

    Add files via upload

--- nn_choice_LSTMadam.m
+++ nn_choice_LSTMadam.m
@@ -7,7 +7,7 @@ load("traindata.mat")
 
 % 提取特征和标签
 numSamples = length(allData);
-maxFocusPlotLength = 40;
+maxFocusPlotLength = 60;
 
 inputs = [];
 targets_choice = [];
@@ -19,17 +19,23 @@ for i = 1:numSamples
     focusplot_expanded = expData.focusplot(:)';
     padded_focusplot = zeros(1, maxFocusPlotLength);
     padded_focusplot(1:length(focusplot_expanded)) = focusplot_expanded;
+
+    ped0dist_expanded = expData.ped0d(:)';
+    padded_ped0dist = zeros(1, maxFocusPlotLength);
+    padded_ped0dist(1:length(ped0dist_expanded)) = ped0dist_expanded;
+
+    ped1dist_expanded = expData.ped1d(:)';
+    padded_ped1dist = zeros(1, maxFocusPlotLength);
+    padded_ped1dist(1:length(ped1dist_expanded)) = ped1dist_expanded;
     % 将所有输入特征拼接成一个行向量
-    input = [expData.reactiontime, expData.ped0val, expData.ped1val, expData.startlane, padded_focusplot];
+    input = [expData.reactiontime, expData.ped0val, expData.ped1val, expData.startlane, padded_focusplot, padded_ped0dist, padded_ped1dist];
     startlane = [startlane;expData.startlane];
     inputs = [inputs; input];
     % 将choice转换为分类标签
     if expData.choice == -1
         targets_choice = [targets_choice; 1];
-    elseif expData.choice == 0
-        targets_choice = [targets_choice; 2];
     else
-        targets_choice = [targets_choice; 3];
+        targets_choice = [targets_choice; 2];
     end
 end
 
@@ -64,8 +70,8 @@ X_test_Cell = num2cell(X_test, 1)';
 
 % 设置参数
 inputSize = size(X_train, 1);
-numHiddenUnits = 50; % 减少LSTM层的单元数
-dropoutRate = 0.7; % 增加dropout比例
+numHiddenUnits = 100; % 减少LSTM层的单元数
+dropoutRate = 0.6; % 增加dropout比例
 globalL2RegFactor = 0.1; % 全局L2正则化系数
 layerL2RegFactor = 0.1; % 层特定的L2正则化因子
 
@@ -73,17 +79,17 @@ layers_classification = [
     sequenceInputLayer(inputSize, 'Name', 'input')
     lstmLayer(numHiddenUnits, 'OutputMode', 'last', 'Name', 'lstm1')
     dropoutLayer(dropoutRate, 'Name', 'dropout')
-    fullyConnectedLayer(3, 'Name', 'choice_fc', 'WeightL2Factor', layerL2RegFactor, 'BiasL2Factor', layerL2RegFactor) % 使用L2正则化因子
+    fullyConnectedLayer(2, 'Name', 'choice_fc', 'WeightL2Factor', layerL2RegFactor, 'BiasL2Factor', layerL2RegFactor) % 使用L2正则化因子
     softmaxLayer('Name', 'choice_softmax')
     classificationLayer('Name', 'choice_classification')];
 
 % 创建并训练分类任务的神经网络
 options_classification = trainingOptions('adam', ...
-    'MaxEpochs', 2000, ... % 增加训练迭代次数
-    'MiniBatchSize', 128, ... % 调整批量大小
+    'MaxEpochs', 1000, ... % 增加训练迭代次数
+    'MiniBatchSize', 64, ... % 调整批量大小
     'InitialLearnRate', 1e-4, ... % 减少初始学习率
     'LearnRateSchedule', 'piecewise', ...
-    'LearnRateDropFactor', 0.25, ...
+    'LearnRateDropFactor', 0.2, ...
     'LearnRateDropPeriod', 250, ...
     'L2Regularization', globalL2RegFactor, ... % 全局L2正则化系数
     'ValidationData', {X_test_Cell, valTargets_choice_categorical}, ... % 设置验证数据
@@ -99,8 +105,8 @@ net_classification = trainNetwork(X_train_Cell, trainTargets_choice_categorical,
 
 % 预测分类结果
 YPred_classification = classify(net_classification, X_test_Cell);
-predicted_choice = double(YPred_classification) - 2; % 将标签转换为-1, 0, 1
-gt_choice = valTargets_choice-2;
+predicted_choice = double(YPred_classification)*2 - 3; % 将标签转换为-1,  1
+gt_choice = valTargets_choice*2-3;
 % 计算分类准确率
 accuracy_choice = sum(predicted_choice == gt_choice) / numel(gt_choice);
 accuracy_choice_num = sum(predicted_choice == gt_choice);
